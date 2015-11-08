390e268661b5b585204683bb44b3c37a278e4178
Michael Herman <hermanmu@gmail.com>
2015-11-08 08:18:21 -0700

    updated tests

--- tests/base.py
+++ tests/base.py
@@ -14,6 +14,8 @@ class BaseTestCase(TestCase):
         return app
 
     def setUp(self):
+        db.session.remove()
+        db.drop_all()
         db.create_all()
         studentUser = User(
             email="student@student.com",
