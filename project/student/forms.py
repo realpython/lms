bdc4558c46589324c3d92c2f7f48b7856fc071d1
Michael Herman <hermanmu@gmail.com>
2015-11-08 19:59:57 -0700

    refactored add course form

--- project/student/forms.py
+++ project/student/forms.py
@@ -4,22 +4,6 @@
 from flask_wtf import Form
 from wtforms import SelectField
 
-from project.models import Course
-
-
-def get_courses():
-    courses = Course.query.all()
-    if courses:
-        return courses
-
 
 class AddCourseForm(Form):
-    get_all_courses = get_courses()
-    if get_all_courses:
-        courses = SelectField(
-            u"Available Courses",
-            choices=[
-                (single_course.name, single_course.name)
-                for single_course in get_all_courses
-            ]
-        )
+    courses = SelectField(u"Available Courses")
--- project/student/views.py
+++ project/student/views.py
@@ -9,9 +9,10 @@ from flask import render_template, Blueprint, request, flash, redirect
 from flask.ext.login import login_required
 from flask.ext.login import current_user
 
+
+from project.student.forms import AddCourseForm
 from project import db
 from project.models import Course, User
-# from project.student.forms import AddCourseForm
 
 
 ##########
@@ -25,8 +26,10 @@ student_blueprint = Blueprint('student', __name__,)
 # helpers #
 ###########
 
-def get_courses(user_id):
-    pass
+def get_all_courses():
+    courses = Course.query.all()
+    if courses:
+        return courses
 
 
 def get_single_course(course_name):
@@ -53,6 +56,10 @@ def show_courses():
 @login_required
 def add_course():
     form = AddCourseForm(request.form)
+    form.courses.choices = [
+        (single_course.name, single_course.name)
+        for single_course in get_all_courses()
+    ]
     if form.validate_on_submit():
         course = get_single_course(form.courses.data)
         user = User.query.filter_by(id=current_user.get_id()).first()
