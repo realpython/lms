4095fce2835575d0bb9230975dc84e210eac1fa3
Michael Herman <hermanmu@gmail.com>
2015-11-10 08:29:13 -0700

    updated tests

--- project/config.py
+++ project/config.py
@@ -21,7 +21,7 @@ class DevelopmentConfig(BaseConfig):
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG_TB_ENABLED = False
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
