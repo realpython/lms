74de86a4890f570a6d1eb44b98dca946f0e786d9
Michael Herman <hermanmu@gmail.com>
2015-12-15 09:59:49 -0700

    added coveralls

--- manage.py
+++ manage.py
@@ -42,16 +42,20 @@ def test():
 def cov():
     """Runs the unit tests with coverage."""
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    COV.stop()
-    COV.save()
-    print('Coverage Summary:')
-    COV.report()
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    covdir = os.path.join(basedir, 'tmp/coverage')
-    COV.html_report(directory=covdir)
-    print('HTML version: file://%s/index.html' % covdir)
-    COV.erase()
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+        return 0
+    else:
+        return 1
 
 
 @manager.command
